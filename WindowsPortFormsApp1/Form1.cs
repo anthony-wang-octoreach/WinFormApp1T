27322afd2251a15927475496950d0e6385b1d105
DESKTOP-MM8MHMU\anthony <anthony.wang@octoreach.com>
2022-12-19 17:05:25 +0800

    test commit 1

--- WindowsPortFormsApp1/Form1.cs
+++ WindowsPortFormsApp1/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
-
+//haha
 namespace WindowsPortFormsApp1
 {
     public partial class Form1 : Form
